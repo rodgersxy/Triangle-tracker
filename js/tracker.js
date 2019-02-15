a407744e5b1831f963d1c7181cdae24d05762cd7
rodgers <rodgersny99@gmail.com>
2019-02-15 17:35:44 +0300

    README.md file

---
+++ js/tracker.js
@@ -0,0 +1,17 @@
+
+function display() {
+  var lengthX = parseInt(document.getElementById('lengthX').value);
+  var lengthY = parseInt(document.getElementById('lengthY').value);
+  var lengthZ = parseInt(document.getElementById('lengthZ').value);
+
+  if (lengthX + lengthY <= lengthZ || lengthY + lengthZ <= lengthX || lengthX + lengthZ <= lengthY){
+    alert("not a TRIANGLE");
+  }else if (lengthX==lengthY && lengthY==lengthZ && lengthZ==lengthX){
+    alert( "this is an equilateral triangle");
+  }else if (lengthX==lengthY || lengthY==lengthZ || lengthZ==lengthX ){
+    alert ("this is an isosceles triangle");
+  }else {
+    alert( "this is a scalene triangle"):
+  }
+}
+display();
